b0bdac397e2cf49bcd3ef76d26a60e20b55f2b49
Jennifer <jennifertorchelsen@gmail.com>
2023-04-06 16:27:39 -0300

    Pesquisar geral de informações

--- controllers/jogadorController.js
+++ controllers/jogadorController.js
@@ -2,107 +2,135 @@ import { sequelize } from "../databases/conecta.js";
 import { Jogador } from "../models/Jogador.js";
 import { Op } from "sequelize";
 
-
 export const jogadorIndex = async (req, res) => {
-    try {
-      const jogadores = await Jogador.findAll();
-      res.status(200).json(jogadores)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+  try {
+    const jogadores = await Jogador.findAll();
+    res.status(200).json(jogadores);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const jogadorCreate = async (req, res) => {
+  const { nome, clube, posicao, salario, idade } = req.body;
+
+  // se não informou estes atributos
+  if (!nome || !clube || !posicao || !salario || !idade) {
+    res
+      .status(400)
+      .json({
+        id: 0,
+        msg: "Erro... Informe nome, clube, posicao, idade e salario do jogador.",
+      });
+    return;
   }
 
-  export const jogadorCreate = async (req, res) => {
-    const { nome, clube, posicao, salario,idade } = req.body
-  
-    // se não informou estes atributos
-    if (!nome || !clube || !posicao || !salario || !idade) {
-      res.status(400).json({ id: 0, msg: "Erro... Informe nome, clube, posicao, idade e salario do jogador." })
-      return
-    }
-  
-    try {
-      const jogador = await Jogador.create({
-        nome, clube, posicao, salario, idade
-      });
-      res.status(201).json(jogador)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+  try {
+    const jogador = await Jogador.create({
+      nome,
+      clube,
+      posicao,
+      salario,
+      idade,
+    });
+    res.status(201).json(jogador);
+  } catch (error) {
+    res.status(400).send(error);
   }
-  
+};
+
 export const jogadorUpdate = async (req, res) => {
-    const { id } = req.params
-    const { nome, clube, posicao, idade, salario } = req.body
-  
-    if (!nome || !clube || !posicao || !idade || !salario) {
-      res.status(400).json({ id: 0, msg: "Erro... Informe nome, clube, posicao e idade do jogador." })
-      return
-    }
-  
-    try {
-      const jogador = await Jogador.update({
-        nome, clube, posicao, idade, salario
-      }, {
-        where: { id }
+  const { id } = req.params;
+  const { nome, clube, posicao, idade, salario } = req.body;
+
+  if (!nome || !clube || !posicao || !idade || !salario) {
+    res
+      .status(400)
+      .json({
+        id: 0,
+        msg: "Erro... Informe nome, clube, posicao e idade do jogador.",
       });
-      res.status(200).json(jogador)
-    } catch (error) {
-      res.status(400).send(error)
-    }
-
-  }
-  export const jogadorDestroy = async (req, res) => {
-    const { id } = req.params
-    try {
-      const jogador = await Jogador.destroy({
-        where: { id }
-      });
-      res.status(200).json(jogador)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+    return;
   }
 
-  
-  export const jogadorNome = async (req, res) => {
-    const { nome } = req.params
-
-    try {
-      const jogadores = await Jogador.findAll({
-        where:{
-            nome:{
-         [Op.like] : '%' + nome + '%'
-            }
-    }
-      })
-      res.status(200).json(jogadores)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+  try {
+    const jogador = await Jogador.update(
+      {
+        nome,
+        clube,
+        posicao,
+        idade,
+        salario,
+      },
+      {
+        where: { id },
+      }
+    );
+    res.status(200).json(jogador);
+  } catch (error) {
+    res.status(400).send(error);
   }
-
-  export const jogadorIdade = async (req, res) => {
-    const { idade } = req.params
-
-    try {
-      const jogadores = await Jogador.findAll({
-        where:{ idade: idade }
-      })
-      res.status(200).json(jogadores)
-    } catch (error) {
-      res.status(400).send(error)
-    }
-  } 
-
-  export const jogadorSalarioDesc = async (req, res) => {
-
-    try {
-      const jogadores = await Jogador.findAll({
-        order: sequelize.col('salario')
-      })
-      res.status(200).json(jogadores)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+};
+export const jogadorDestroy = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const jogador = await Jogador.destroy({
+      where: { id },
+    });
+    res.status(200).json(jogador);
+  } catch (error) {
+    res.status(400).send(error);
   }
+};
+
+export const jogadorNome = async (req, res) => {
+  const { nome } = req.params;
+  //const pesq = req.params.pesq
+  try {
+    const jogadores = await Jogador.findAll({
+      where: {
+        nome: {
+          //[Op.substring]: pesq
+          [Op.like]: "%" + nome + "%",
+        },
+      },
+    });
+    res.status(200).json(jogadores);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const idadeIntervalo = async (req, res) => {
+  const { idadeUm, idadeDois } = req.params;
+
+  try {
+    const jogadores = await Jogador.findAll({
+      where: {
+        idade: {
+          [Op.between]: [idadeUm, idadeDois],
+        },
+      },
+    });
+    res.status(200).json(jogadores);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const pesquisaGeral = async (req, res) => {
+  const { pesquisa } = req.params;
+
+  try{
+    const jogadores = await Jogador.findAll({
+      where:{
+        nome, clube, posicao, idade, salario:{
+          
+        },
+      },
+    });
+    res.status(200).json(jogadores);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { jogadorIndex, jogadorCreate, jogadorUpdate, jogadorDestroy, jogadorNome, jogadorIdade, jogadorSalarioDesc } from "./controllers/jogadorController.js"
+import { jogadorIndex, jogadorCreate, jogadorUpdate, jogadorDestroy, jogadorNome, idadeIntervalo, pesquisaGeral } from "./controllers/jogadorController.js"
 
 const router = Router()
 
@@ -8,7 +8,7 @@ router.get('/jogadores', jogadorIndex)
       .put('/jogadores/:id', jogadorUpdate)
       .delete('/jogadores/:id', jogadorDestroy)
       .get('/jogadores/:nome', jogadorNome)
-      .get('/jogadores/idade/:idade',jogadorIdade)
-      .get('/jogador/salario',jogadorSalarioDesc)
+      .get('/jogadores/pesq/:idadeUm/:idadeDois', idadeIntervalo)
+      .get('/jogadores/pesq/:pesquisa', pesquisaGeral)
 
 export default router
